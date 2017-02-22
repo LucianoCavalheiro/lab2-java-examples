ff2432c7487317bbfc4dace1c7228e344681e998
Luciano Cavalheiro da Silva <luciano.cavalheiro@gmail.com>
2017-02-22 07:09:35 -0300

    Adiciona implementacao inicial dos métodos de Carro e Caminhao.

--- src/lab2/aula01/Caminhao.java
+++ src/lab2/aula01/Caminhao.java
@@ -1,5 +1,44 @@
 package lab2.aula01;
 
 public class Caminhao {
+	private String placa;
+	private String fabricante;
+	private String modelo;
+	private double kmRodados;
+	private double consumoLitrosPorKm;
+	private double cicloRevisaoKm;
+	private double cargaKg;
+	private double autonomiaLitros;
+	private double kmAteProximaRevisao;
+	
 
+	public String getCombustivel() {
+		return "diesel";
+	}
+	
+	public void setCarga(double novaCargaKm) {
+		this.cargaKg = novaCargaKm;
+	}
+	
+	public double estimarConsumo(double distanciaKm) {
+		return distanciaKm / ( this.consumoLitrosPorKm * Math.max(1.0, this.cargaKg/1000.0));
+	}
+	
+	public boolean podePercorrerSemAbastecer(double distanciaKm) {
+		return this.estimarConsumo(distanciaKm) < this.autonomiaLitros;
+	}
+
+	public void rodar(double km) {
+		this.kmRodados += km;
+		this.kmAteProximaRevisao -= km;
+	}
+
+	public boolean requerRevisao() {
+		return this.kmAteProximaRevisao <= 0.0;
+	}
+
+	public void revisaoRealizada() {
+		this.kmAteProximaRevisao = this.cicloRevisaoKm;
+	}
+	
 }
--- src/lab2/aula01/Carro.java
+++ src/lab2/aula01/Carro.java
@@ -1,7 +1,7 @@
 package lab2.aula01;
 
 public class Carro {
-
+	private String placa;
 	private String fabricante;
 	private String modelo;
 	private String cor;
@@ -11,20 +11,31 @@ public class Carro {
 	private String combustivel;
 	private double consumoLitrosPorKm;
 	private double cicloRevisaoKm;
+	private double kmAteProximaRevisao;
+	private double autonomiaLitros;
+	
+	public String getCombustivel() {
+		return this.combustivel;
+	}
 
 	public double estimarConsumo(double distanciaKm) {
-		
+		return distanciaKm / this.consumoLitrosPorKm;
+	}
+	
+	public boolean podePercorrerSemAbastecer(double distanciaKm) {
+		return this.estimarConsumo(distanciaKm) < this.autonomiaLitros;
 	}
 
 	public void rodar(double km) {
-		
-	}
+		this.kmRodados += km;
+		this.kmAteProximaRevisao -= km;
+	}	
 
 	public boolean requerRevisao() {
-		
+		return this.kmAteProximaRevisao <= 0.0;
 	}
 
 	public void revisaoRealizada() {
-		
+		this.kmAteProximaRevisao = this.cicloRevisaoKm;
 	}
 }
